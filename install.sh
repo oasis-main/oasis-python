2a01b21988c90c3a4c3b3451d7400b2a296eb399
Pat C <patrick.w.carroll@gmail.com>
2023-02-17 11:36:36 -0500

    Adding CORS config for nginx

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ sudo systemctl restart nginx
 echo "TLS Request Encryption (HTTPS only...)"
 sudo snap install --classic certbot
 sudo certbot --nginx -d demo.oasis-x.io -d www.demo.oasis-x.io
+sudo cp /home/ubuntu/oasis-python/configs/cors_support /etc/nginx/sites-available/demo.oasis-x.io
 sudo cp /home/ubuntu/oasis-python/configs/oasis_python_secure.nginx /etc/nginx/sites-available/demo.oasis-x.io
 sudo systemctl reload nginx
 sudo ufw allow 'Nginx Full'
