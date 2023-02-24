11b964983f98458b57f61d9edc4d9ae8259c7616
Mike <leemichael289@gmail.com>
2023-02-24 14:41:47 -0500

    Changing filepaths and urls for prod

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-#CLIENT_DOMAIN='https://demo.oasis-x.io' #cloud
-CLIENT_DOMAIN='http://localhost:8502' #local
+CLIENT_DOMAIN='https://demo.oasis-x.io' #cloud
+#CLIENT_DOMAIN='http://localhost:8502' #local
 
 MARKETS_DOMAIN='https://markets.oasis-x.io'
 
 AUTH_DOMAIN='https://auth.oasis-x.io'
 
-OS_PATH = '/Users/Michaellee/Documents/Runes/oasis-x' #local
-#OS_PATH = "/home/ubuntu"
+#OS_PATH = '/Users/Michaellee/Documents/Runes/oasis-x' #local
+OS_PATH = "/home/ubuntu"
 
 CWD = "/oasis-python"
