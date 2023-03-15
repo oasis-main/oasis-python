b3d7ef6630e45cd9df65a3c717cbf2146afe0985
Pat C <patrick.w.carroll@gmail.com>
2023-03-15 16:01:49 -0400

    Adding config values

--- config.py
+++ config.py
@@ -9,3 +9,8 @@ AUTH_DOMAIN='https://auth.oasis-x.io'
 OS_PATH = "/home/ubuntu"
 
 CWD = "/oasis-python"
+
+#Pat's possibly deprecated values
+API_DOMAIN='https://markets.oasis-x.io'
+CLIENT_DOMAIN='https://demo.oasis-x.io'
+ROOT_PATH='/home/bobbytables/Git/oasis-python'
