76771285b247f8a435fd1fa49ff0f345b1767daf
Mike <leemichael289@gmail.com>
2023-03-30 16:42:53 -0400

    Deleted some deprecated variables

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
-CLIENT_DOMAIN='https://demo.oasis-x.io' #cloud
 #CLIENT_DOMAIN='http://localhost:8502' #local
-MARKETS_DOMAIN='http://0.0.0.0:8502'
-#MARKETS_DOMAIN='https://markets.oasis-x.io'
+CLIENT_DOMAIN='https://demo.oasis-x.io' #cloud
 
+#MARKETS_DOMAIN='http://localhost:8502'
+MARKETS_DOMAIN='https://markets.oasis-x.io'
+
+#AUTH_DOMAIN='http://localhost:8502'
 AUTH_DOMAIN='https://auth.oasis-x.io'
 
 #OS_PATH = '/Users/Michaellee/Documents/Runes/oasis-x' #local
+#OS_PATH='/home/bobbytables/Git/oasis-python'
 OS_PATH = "/home/ubuntu"
 
 CWD = "/oasis-python"
-
-#Pat's possibly deprecated values
-API_DOMAIN='https://markets.oasis-x.io'
-CLIENT_DOMAIN='https://demo.oasis-x.io'
-ROOT_PATH='/home/bobbytables/Git/oasis-python'
